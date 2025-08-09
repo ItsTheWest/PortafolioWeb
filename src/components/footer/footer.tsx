2d9e76bfdfdc947efb0559f509e1febb205aa759
Nelson <nelsonjosue0407@gmail.com>
2025-08-09 04:37:29 -0400

    se suben los cambios del footer, menu y apartado de proyecto

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './Footer.css';
+import './footerstyle.css';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>
-          Desarrollado por Nelson &copy; {new Date().getFullYear()} — Transformando ideas en código.
+          Desarrollado por Nelson &copy; {new Date().getFullYear()}  Transformando ideas en código.
         </p>
       </div>
     </footer>
